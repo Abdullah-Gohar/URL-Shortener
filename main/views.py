c613596fa4abd0732145cd8748be673c6b7e7620
Abdullah-Gohar <abdullahgohar01@gmail.com>
2023-05-09 14:51:44 +0500

    Nothing

--- main/views.py
+++ main/views.py
@@ -7,13 +7,10 @@ import random
 
 def index(response):
     if response.method == "POST":
-        print(response.POST["url"])
         shortened = shorten_url(response.POST["url"])
         Url(id =shortened.split('/')[-1] ,link=add_https(response.POST["url"]),shortened=shortened).save()
         return render(response,'main/index.html',{'hidden' : False,'shortened':shortened})
         
-        
-    # return HttpResponse("<h1>Trimly</h1>")
     else:
         return render(response,'main/index.html',{'hidden' : True,'shortened':''})
 
