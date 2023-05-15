57a10a28cc03a3160b688311b068d310f8841a7a
ammaryounas339 <ammaryounas339.ay@gmail.com>
2023-05-16 01:00:17 +0500

    Added URL Validation , only changed index.html and views.py

--- main/views.py
+++ main/views.py
@@ -1,25 +1,27 @@
+from django.forms import URLField
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Url
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 import random
 
 def index(response):
-    if response.method == "POST":
-        shortened = shorten_url(response.POST["url"])
-        Url(id =shortened.split('/')[-1] ,link=add_https(response.POST["url"]),shortened=shortened).save()
-        return render(response,'main/index.html',{'hidden' : False,'shortened':shortened})
-        
+    if response.method == "POST":      
+          if validate_url(response.POST["url"])== True:
+            shortened = shorten_url(response.POST["url"])
+            Url(id =shortened.split('/')[-1] ,link=add_https(response.POST["url"]),shortened=shortened).save()
+            return render(response,'main/index.html',{'hidden' : False,'shortened':shortened})
+          else:
+              return render(response,'main/index.html',{'hidden_error' : False,'shortened':""})
     else:
-        return render(response,'main/index.html',{'hidden' : True,'shortened':''})
-
+         return render(response,'main/index.html',{'hidden' : True,'shortened':''})
 
 def redirect_url(response,id):
     item = Url.objects.get(id=id)
-    
-    
-    return redirect(item.link)
+    return redirect(item.link) 
     
 def shorten_url(url, base_url="www.trimly.info/"):
     while True:
@@ -37,4 +39,11 @@ def add_https(url:str):
             return "https://"+url
         else:
             return "https://www."+url
-            
+        
+def validate_url(url):
+    url_form_field = URLField()
+    try:
+        url = url_form_field.clean(url)
+    except ValidationError:
+        return False
+    return True
