fa15bb25f883c50a6e981164521b958f49c982ef
Abdullah-Gohar <abdullahgohar01@gmail.com>
2023-05-15 23:07:45 +0500

    Added unsecured version of copy for http

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -2,11 +2,26 @@
 function copyDivToClipboard() {
         // Get the text field
         var copyText = document.getElementById("shortened-url").innerText;
-        navigator.clipboard.writeText(copyText);
+        unsecuredCopyToClipboard(copyText);
+        // navigator.clipboard.writeText(copyText);
 
-        // Alert the copied text
-        alert("Copied the text: " + copyText);
+        // // Alert the copied text
+        // alert("Copied the text: " + copyText);
 } 
+
+function unsecuredCopyToClipboard(text) {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+                document.execCommand('copy');
+        } catch (err) {
+                console.error('Unable to copy to clipboard', err);
+        }
+        document.body.removeChild(textArea);
+}
 
 function scrollToSection(id){
         const section = document.getElementById(id);
